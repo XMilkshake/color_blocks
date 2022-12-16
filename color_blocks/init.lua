ff761c2f2b188e4405c439e51b1b74dbdc8cd1b1
milkshake <120337338+XMilkshake@users.noreply.github.com>
2022-12-16 22:43:06 +0300

    Add files via upload

---
+++ color_blocks/init.lua
@@ -0,0 +1,271 @@
+--made by Milkshake.--
+
+--Black block.--
+
+minetest.register_node("color_blocks:black", {
+  description = "Black block.",
+  tiles = { name = "black.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:black",
+  recipe = {
+    {"","",""},
+    {"","default:wood","dye:black"},
+    {"","",""},
+  },
+})
+
+--Blue block.--
+
+minetest.register_node("color_blocks:blue", {
+  description = "Blue block.",
+  tiles = { name = "blue.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:blue",
+  recipe = {
+    {"","",""},
+    {"","default:wood","dye:blue"},
+    {"","",""},
+  },
+})
+
+--Brown block.--
+
+minetest.register_node("color_blocks:brown", {
+  description = "Brown block.",
+  tiles = { name = "brown.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:brown",
+  recipe = {
+    {"","",""},
+    {"","default:wood","dye:brown"},
+    {"","",""},
+  },
+})
+
+--Cyan block.--
+
+minetest.register_node("color_blocks:brown", {
+  description = "Cyan block.",
+  tiles = { name = "cyan.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:brown",
+  recipe = {
+    {"","",""},
+    {"","default:wood","dye:cyan"},
+    {"","",""},
+  },
+})
+
+--Grey block.--
+
+minetest.register_node("color_blocks:grey", {
+  description = "Grey block.",
+  tiles = { name = "grey.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:grey",
+  recipe = {
+    {"","",""},
+    {"","default:wood","dye:grey"},
+    {"","",""},
+  },
+})
+
+--Green block.--
+
+minetest.register_node("color_blocks:green", {
+  description = "Green block.",
+  tiles = { name = "green.png" },
+  drawtype = "nodebox",
+  groups  = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:green",
+  recipe = {
+    {"","",""},
+    {"","default:wood","dye:green"},
+    {"","",""},
+  },
+})
+
+--Lime block.--
+
+minetest.register_node("color_blocks:lime", {
+  description = "Lime block.",
+  tiles = { name = "lime.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:lime",
+  recipe = {
+    {"","",""},
+    {"dye:white","default:wood","dye:green"},
+    {"","",""},
+  },
+})
+
+--Magenta block.--
+
+minetest.register_node("color_blocks:magenta", {
+  description = "magenta block.",
+  tiles = { name = "magenta.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:magenta",
+  recipe = {
+    {"","",""},
+    {"","default:wood","dye:magenta"},
+    {"","",""},
+  },
+})
+
+--Orange block.--
+
+minetest.register_node("color_blocks:orange", {
+  description = "Orange block.",
+  tiles = { name = "orange.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:orange",
+  recipe = {
+    {"","",""},
+    {"","default:wood","dye:orange"},
+    {"","",""},
+  },
+})
+
+--Pink block.--
+
+minetest.register_node("color_blocks:pink", {
+  description = "Pink block.",
+  tiles = { name = "pink.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:pink",
+  recipe = {
+    {"","",""},
+    {"","default:wood","dye:pink"},
+    {"","",""},
+  },
+})
+
+--Purple block.--
+
+minetest.register_node("color_blocks:purple", {
+  description = "Purple block.",
+  tiles = { name = "purple.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:purple",
+  recipe = {
+    {"","",""},
+    {"","default:wood","dye:violet"},
+    {"","",""},
+  },
+})
+
+--Red block.--
+
+minetest.register_node("color_blocks:red", {
+  description = "Red block.",
+  tiles = { name = "red.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:red",
+  recipe = {
+    {"","",""},
+    {"","default:wood","dye:red"},
+    {"","",""},
+  },
+})
+
+--Silver block.--
+
+minetest.register_node("color_blocks:silver", {
+  description = "Silver color block.",
+  tiles = { name = "silvercolorblock.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:silver",
+  recipe = {
+    {"","",""},
+    {"dye:white","default:wood","dye:grey"},
+    {"","",""},
+  },
+})
+
+--White block.--
+
+minetest.register_node("color_blocks:white", {
+  description = "White block.",
+  tiles = { name = "white.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:white",
+  recipe = {
+    {"","",""},
+    {"","default:wood","dye:white"},
+    {"","",""},
+  },
+})
+
+--Yellow block.--
+
+minetest.register_node("color_blocks:yellow", {
+  description = "Yellow block.",
+  tiles = { name = "yellow.png" },
+  drawtype = "nodebox",
+  groups = { oddly_breakable_by_hand = 3 },
+})
+
+minetest.register_craft({
+  output = "color_blocks:yellow",
+  recipe = {
+    {"","",""},
+    {"","default:wood","dye:yellow"},
+    {"","",""},
+  },
+})
